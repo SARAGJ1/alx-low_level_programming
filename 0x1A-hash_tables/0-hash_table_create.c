925f8e35dcab9faa666df29973100683fdb9e0ab
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-02-24 19:32:30 +0000

    comit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,6 +3,13 @@
 #include <stdio.h>
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - hash table creation
+ * @size: size of the table
+ *
+ * return pointer to table if sucessed 
+ * or NULL if failed
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
@@ -12,5 +19,5 @@ hash_table_t *hash_table_create(unsigned long int size)
 	{
 		return (NULL);
 	}
-	return table;
+	return (table);
 }
