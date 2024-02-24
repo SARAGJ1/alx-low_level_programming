029dc2b21c219583bf4c1b73fb792bd2ba500aaf
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-02-24 19:48:58 +0000

    commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,13 +14,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
 
-	table = malloc((sizeof(hash_table_t));
+	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 	{
 		return (NULL);
 	}
-	table->buckets = malloc(size * sizeof(hash_node_t *));
-	if (table->buckets == NULL)
+	table->array = malloc(size * sizeof(hash_node_t *));
+	if (table->array == NULL)
 	{
 		return (NULL);
 	}
