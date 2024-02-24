a1463823774c5292c5716001b26643a544b6b1af
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-02-24 19:12:11 +0000

    comit

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+
+	table = malloc(size);
+	if (table == NULL)
+	{
+		return (NULL);
+	}
+	return table;
+}
