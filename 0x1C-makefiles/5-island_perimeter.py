19e446f48c592afeb931bc726aedcdd74f9de23c
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-04-12 00:12:09 +0000

    hh

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!usr/bin/python3
+""" returns the perimeter of the island described in grid"""
+
+
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid"""
+
+    w = len(grid[0])
+    h = len(grid)
+    e = 0
+    size = 0
+    k = 0
+
+    for i in range(h):
+        for j in range(w):
+            if grid[i][j] == 1:
+                size = size + 1
+                if (j > 0 and grid[i][j - 1] == 1) or (j > 0 and grid[i - 1][j] == 1):
+                        e += 1
+    k = size * 4 - e * 2
+    return k
