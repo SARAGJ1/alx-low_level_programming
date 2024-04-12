54f037bf479831e8f084c85f0c90fccbbc3e526f
SARAGJ1 <159816313+SARAGJ1@users.noreply.github.com>
2024-04-12 02:17:18 +0200

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -15,7 +15,7 @@ def island_perimeter(grid):
         for j in range(w):
             if grid[i][j] == 1:
                 size = size + 1
-                if (j > 0 and grid[i][j - 1] == 1) or (j > 0 and grid[i - 1][j] == 1):
+                if (j > 0 and grid[i][j - 1] == 1) or (i > 0 and grid[i - 1][j] == 1):
                         e += 1
     k = size * 4 - e * 2
     return k
