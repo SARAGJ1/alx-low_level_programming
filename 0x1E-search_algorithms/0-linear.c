9265ef09b83177ba9b45c4823b50441ed1cf4fc1
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-05-22 22:35:08 +0000

    commit

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in an array
+ * @i : internal variable
+ *
+ * Return: index i if SUCCESS
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+		{
+			return ((int)i);
+		}
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+	}
+	return (-1);
+}
